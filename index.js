933eec1de5abea92e3d3bf6eb696215c1881f44c
Rashadat Mirzayev <334rashad@gmail.com>
2021-09-14 22:55:15 +0200

    more changes, click on footballers with div, instead of map

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
+// window.onload = function () {
+//   var ImageMap = function (map) {
+//       var n,
+//         areas = map.getElementsByTagName("area"),
+//         len = areas.length,
+//         coords = [],
+//         previousWidth = 562;
+//       for (n = 0; n < len; n++) {
+//         coords[n] = areas[n].coords.split(",");
+//       }
+//       this.resize = function () {
+//         var n,
+//           m,
+//           clen,
+//           x = document.body.clientWidth / previousWidth;
+//         for (n = 0; n < len; n++) {
+//           clen = coords[n].length;
+//           for (m = 0; m < clen; m++) {
+//             coords[n][m] *= x;
+//           }
+//           areas[n].coords = coords[n].join(",");
+//         }
+//         previousWidth = document.body.clientWidth;
+//         return true;
+//       };
+//       window.onresize = this.resize;
+//     },
+//     imageMap = new ImageMap(document.getElementById("ronaldo"));
+//   imageMap.resize();
+// };
+
 const footballer = (name) => {
   const popup = document.getElementById("popup_p");
   document.getElementById(
     "footballer_info"
   ).innerHTML = `<span> information about: ${name} </span>`;
   popup.classList.toggle("show");
-  event.stopPropagation();
+  // event.stopPropagation();
 };
 
 // const el = document.querySelector("#ronaldo");
