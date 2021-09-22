3fd3b46cc13021376024ee7190e9e4f0ce4d0f23
Rashadat Mirzayev <334rashad@gmail.com>
2021-09-22 11:50:14 +0200

    home page almost ready, fonts changed, logo, containers

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const footballer = (name) => {
+  const popup = document.getElementById("popup_p");
+  document.getElementById(
+    "footballer_info"
+  ).innerHTML = `<span> information about: ${name} </span>`;
+  popup.classList.toggle("show");
+  // event.stopPropagation();
+};
+
 // window.onload = function () {
 //   var ImageMap = function (map) {
 //       var n,
@@ -28,22 +37,3 @@
 //     imageMap = new ImageMap(document.getElementById("ronaldo"));
 //   imageMap.resize();
 // };
-
-const footballer = (name) => {
-  const popup = document.getElementById("popup_p");
-  document.getElementById(
-    "footballer_info"
-  ).innerHTML = `<span> information about: ${name} </span>`;
-  popup.classList.toggle("show");
-  // event.stopPropagation();
-};
-
-// const el = document.querySelector("#ronaldo");
-// if (el) {
-//   el.addEventListener("mouseover", mouseOver);
-// } else alert("not working");
-
-// function mouseOver() {
-//   document.getElementsByClassName("second_row")[0].style.backgroundColor =
-//     "red";
-// }
