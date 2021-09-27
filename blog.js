ac836af0e8470607af18d402b5a9fe1711482a99
Rashadat Mirzayev <334rashad@gmail.com>
2021-09-28 01:26:20 +0200

    open close article

--- blog.js
+++ blog.js
@@ -1,7 +1,12 @@
 const header1 = document.getElementsByTagName("h1");
+const popupArticle = document.getElementById("popup_article");
 
 const openArticle = (number) => {
-  const popupArticle = document.getElementById("popup_article");
   header1[0].style.display = "none";
   popupArticle.style.display = "block";
+};
+
+const closeArticle = () => {
+  header1[0].style.display = "block";
+  popupArticle.style.display = "none";
 };
