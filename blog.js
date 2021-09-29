fd5b8ec9506c29d80548480b0b04dfc00c322727
Rashadat Mirzayev <334rashad@gmail.com>
2021-09-29 23:18:12 +0200

    Blog page, click any article to open, iterate through other articles to close

--- blog.js
+++ blog.js
@@ -1,13 +1,23 @@
+// necessary variables
 const header1 = document.getElementsByTagName("h1");
-const popupArticle = document.getElementById("popup_article");
+const popupArticles = document.getElementsByClassName("popup_article");
+let openedArticle = 0;
 
+console.log(popupArticles);
+// to hide h1 element and open clicked article
 const openArticle = (number) => {
+  openedArticle = number - 1;
   header1[0].style.display = "none";
-  popupArticle.style.display = "block";
-  popupArticle.scrollIntoView();
+  for (var i = 0; i < popupArticles.length; i++) {
+    popupArticles[i].style.display = "none";
+  }
+  popupArticles[openedArticle].style.display = "block";
+  popupArticles[openedArticle].scrollIntoView();
 };
 
-const closeArticle = () => {
+// closes currently open aricle and shows h1 element
+const closeArticle = (number) => {
   header1[0].style.display = "block";
-  popupArticle.style.display = "none";
+  popupArticles[openedArticle].style.display = "none";
+  document.getElementsByTagName("h1")[0].scrollIntoView();
 };
